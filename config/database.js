002f5578a2aa8518d1d959f405ef50c1e8f7b02c
Amy Jordan <amy.jordan@bt.com>
2024-11-26 13:16:32 +0000

    connect to 34sp db

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
+
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'book_reviews',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../config/database");
-const reviewController = require("../controllers/reviewController");
 
 router.get("/", async (req, res) => {
   try {
