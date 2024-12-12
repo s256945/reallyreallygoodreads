e14daa72d2b85247f0e49bbcc274deb37f2c1470
s256945 <143487943+s256945@users.noreply.github.com>
2024-12-12 09:56:39 +0000

    Update authMiddleware.js

--- routes/authMiddleware.js
+++ routes/authMiddleware.js
@@ -1,16 +1,16 @@
-module.exports = {
-  authMiddleware: (req, res, next) => {
-    res.locals.user = req.session.user;
-    if (!req.session.user) {
-      return res.redirect("/login");
-    }
-    next();
-  },
-
-  isAdmin: (req, res, next) => {
-    if (!req.session.user || req.session.user.role !== "admin") {
-      return res.status(403).send("Access denied. Admins only.");
-    }
-    next();
-  },
+const authMiddleware = (req, res, next) => {
+  if (!req.session.user) {
+    return res.redirect("/login");
+  }
+  res.locals.user = req.session.user;
+  next();
 };
+
+const isAdmin = (req, res, next) => {
+  if (!req.session.user || req.session.user.role !== "admin") {
+    return res.status(403).send("Access denied. Admins only.");
+  }
+  next();
+};
+
+module.exports = { authMiddleware, isAdmin };
